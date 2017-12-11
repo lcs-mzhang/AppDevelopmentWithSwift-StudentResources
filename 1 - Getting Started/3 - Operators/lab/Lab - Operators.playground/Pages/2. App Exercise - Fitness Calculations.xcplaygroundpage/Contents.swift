c6fb6debc092bb1bef9e0687a4ed5c276a9fdb5e
Zhang Matt <mzhang@lcs.on.ca>
2017-12-11 09:58:30 -0500

    Completed chapter 3

--- 1 - Getting Started/3 - Operators/lab/Lab - Operators.playground/Pages/2. App Exercise - Fitness Calculations.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/3 - Operators/lab/Lab - Operators.playground/Pages/2. App Exercise - Fitness Calculations.xcplaygroundpage/Contents.swift
@@ -16,7 +16,7 @@ let averageHR = addedHR/3
 print(averageHR)
 
 /*:
- Now create three more constants, `heartRate1D`, `heartRate2D`, and `heartRate3D`, equal to the same values as `heartRate1`, `heartRate2`, and `heartRate3`. These new constants should be of type `Double`. Create a constant `addedHRD` equal to the sum of all three heart rates. Create a constant called `averageHRD` that equals the `addedHRD` divided by 3 to get the average of your new heart rate constants. Print the result. Does this differ from your previous average? Why or why not?
+ Now create three more constants, `heartRate1D`, `heartRate2D`, and `heartRate3D`, equal to the same values as `heartRate1`, `heartRate2`, and `heartRate3`. These new constants should be of type `Double`. Create a constant `addedHRD` equal to the sum of all three heart rates . Create a constant called `averageHRD` that equals the `addedHRD` divided by 3 to get the average of your new heart rate constants. Print the result. Does this differ from your previous average? Why or why not?
  */
 let heartRate1D = 60.0
 let heartRate2D = 80.0
@@ -26,13 +26,14 @@ let addedHRD = heartRate1D + heartRate2D + heartRate3D
 
 let averageHRD = addedHRD/3
 
-print(averageHR)
+print(averageHRD)
 
-//These values differ because
 
 /*:
  Imagine that partway through the day a user has taken 3,467 steps out of the 10,000 step goal. Create constants `steps` and `goal`. Both will need to be of type `Double` so that you can perform accurate calculations. `steps` should be assigned the value 3,467, and `goal` should be assigned 10,000. Create a constant `percentOfGoal` that equals an expression that evaluates to the percent of the goal that has been achieved so far.
  */
-
+let steps = 3467.0
+let goal = 10000.0
+let percentOfGoal = (steps/goal)*100
 
 //: [Previous](@previous)  |  page 2 of 8  |  [Next: Exercise - Compound Assignment](@next)
